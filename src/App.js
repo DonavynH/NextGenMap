bfc3e14e8e535a247a924d8b80ff5fe152bc9dcb
dhaley <dhaley@teksystems.com>
2022-08-17 09:11:33 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Next Gen Map</h1>
+      <iframe src="https://www.google.com/maps/d/embed?mid=1sVWzpIIm0Q01WIVcstXAV0_yTWYiljo&ehbc=2E312F" width="70%" height="680"></iframe>
+    </div>
+  );
+}
+
+export default App;
